f50fb034854cf5ad73c3463267848b71f1d2a81f
Chase <g.chasebutterfield@gmail.com>
2014-09-16 22:27:45 -0600

    End of Koans Day 1

--- koans/devMtnFunctions.js
+++ koans/devMtnFunctions.js
@@ -1,19 +1,20 @@
 describe('First Functions', function(){
   it('should create a function returns the current year', function(){
-    //Write a function called getYear that returns the current year
-
+    var getYear = function() {
+    return 2014;}
     expect(getYear()).toBe(2014);
   });
   it('should create a function returns the current year and saves it', function(){
-    //Write a function that returns the current year. Then call that function and save the returned value
-    //to a variable called currentYearVar
-
-
+    var current = function() {
+      return 2014;}
+    var currentYearVar = current ();
     expect(currentYearVar).toBe(2014);
   });
   it('should create a multiply function', function(){
     //Write a function called multiply that takes in two parametrs, multiplies them together, and returns the answer
-
+    var multiply = function(num1, num2) {
+    return num1*num2;
+    }
     expect(multiply(2,4)).toBe(8);
   });
 })
